7872ab9eb600bd77d1946037f52e1531dd6d8d35
gilberto-maess <gilberto@maess.com.br>
2024-02-25 19:59:12 -0300

    feat: comando para remover um usuário de um evento

---
+++ src/com/pratique/commands/eventos/removerUsuario/IRemoverUsuarioService.java
@@ -0,0 +1,10 @@
+package com.pratique.commands.eventos.removerUsuario;
+
+import java.io.IOException;
+
+import com.pratique.commands.eventos.inscreverUsuario.InscreverUsuarioException;
+import com.pratique.domain.eventos.EventoException;
+
+public interface IRemoverUsuarioService {
+	void Executar(String idUsuario, String idEvento) throws InscreverUsuarioException, IOException, EventoException;
+}
---
+++ src/com/pratique/commands/eventos/removerUsuario/RemoverUsuarioService.java
@@ -0,0 +1,54 @@
+package com.pratique.commands.eventos.removerUsuario;
+
+import java.io.IOException;
+
+import com.pratique.commands.eventos.inscreverUsuario.InscreverUsuarioException;
+import com.pratique.domain.eventos.Evento;
+import com.pratique.domain.eventos.EventoException;
+import com.pratique.domain.usuarios.Usuario;
+import com.pratique.infra.repositories.eventos.IEventoRepository;
+import com.pratique.infra.repositories.usuarios.IUsuarioRepository;
+import com.pratique.shared.StringHelper;
+
+public class RemoverUsuarioService implements IRemoverUsuarioService {
+
+	private IUsuarioRepository usuarioRepository;
+	private IEventoRepository eventoRepository;
+	
+	public RemoverUsuarioService(
+		IUsuarioRepository usuarioRepository, 
+		IEventoRepository eventoRepository
+	) {
+		this.usuarioRepository = usuarioRepository;
+		this.eventoRepository = eventoRepository;
+	}
+	
+	public void Executar(String idUsuario, String idEvento) throws InscreverUsuarioException, IOException, EventoException {
+		Validar(idUsuario, idEvento);
+		
+		Usuario usuario = usuarioRepository.getId(idUsuario);
+		if (usuario == null) {
+			throw new InscreverUsuarioException("O usuário informado não existe");
+		}
+		
+		Evento evento = eventoRepository.getId(idUsuario);
+		if (evento == null) {
+			throw new InscreverUsuarioException("O evento informado não existe");
+		}
+		
+		evento.removerUsuario(usuario);
+		
+		eventoRepository.update(evento);
+	}
+	
+	private void Validar(String idUsuario, String idEvento) throws InscreverUsuarioException {
+		if (StringHelper.isNullOrEmpty(idUsuario)) {
+			throw new InscreverUsuarioException("O campo idUsuario é obrigatório");
+		}
+		
+		if (StringHelper.isNullOrEmpty(idEvento)) {
+			throw new InscreverUsuarioException("O campo idEvento é obrigatório");
+		}
+	}
+	
+}
